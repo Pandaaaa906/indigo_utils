cb01480274c792faaaae9e340e304db7be37b56c
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2021-03-30 14:26:07 +0800

    add sep_formula_from_obj

--- indigo_utils/formulas.py
+++ indigo_utils/formulas.py
@@ -34,7 +34,7 @@ def formula_from_obj(m: IndigoObject, symbol_ordering=None):
 
 
 def sep_formula_from_obj(m: IndigoObject, sep=' ', ordering=-1):
-    smiles = m.canonicalSmiles()
+    smiles, = m.canonicalSmiles().split(' ')
     ss = smiles.split('.')
     ms = sorted((_indigo.loadMolecule(s) for s in ss), key=lambda tmp: ordering*tmp.molecularWeight())
     return sep.join((formula_from_obj(m) for m in ms))
