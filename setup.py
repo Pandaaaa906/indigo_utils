c77f76c1d01db2eb7b55d4630c32d9d5ee33d551
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2021-08-10 11:45:45 +0800

    fix formula error

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="indigo_utils",
-    version="0.0.1",
+    version="0.0.2",
     author="Pandaaaa906",
     author_email="ye.pandaaaa906@gmail.com",
     description="A small useful tools for epam.indigo",
