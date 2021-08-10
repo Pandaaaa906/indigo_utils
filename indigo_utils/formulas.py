39a185851af14ac496a21d38b58421feec59d2b7
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2021-08-10 11:38:24 +0800

    fix formula error

--- indigo_utils/formulas.py
+++ indigo_utils/formulas.py
@@ -1,16 +1,8 @@
+from typing import Callable
+
 from indigo import Indigo, IndigoObject
 
 _indigo = Indigo()
-
-SYMBOL_ORDERING = {
-    'C': '0',
-    'H': '2',
-    'Cl': '3',
-    'N': '4',
-    'O': '5',
-    'P': '6',
-    'S': '7',
-}
 
 
 def symbol_wrapper(symbol, isotope):
@@ -24,25 +16,33 @@ def symbol_wrapper(symbol, isotope):
         return symbol
 
 
-def formula_from_obj(m: IndigoObject, symbol_ordering=None):
-    symbol_ordering = SYMBOL_ORDERING if symbol_ordering is None else symbol_ordering
+def hill_ordering(symbol: str):
+    if symbol == 'C':
+        return '0'
+    if symbol == 'H':
+        return '1'
+    return symbol.lower()
+
+
+def formula_from_obj(m: IndigoObject, symbol_ordering: Callable = None):
+    symbol_ordering = hill_ordering if symbol_ordering is None else symbol_ordering
     m.unfoldHydrogens()
     d = {}
     for atom in m.iterateAtoms():
         key = (atom.symbol(), atom.isotope())
         d[key] = d.get(key, 0) + 1
-    atoms = sorted(d.items(), key=lambda x: (symbol_ordering.get(x[0][0], x[0][0]), x[0][1]))
+    atoms = sorted(d.items(), key=lambda x: (symbol_ordering(x[0][0]), x[0][1]))
     return ''.join((f'{symbol_wrapper(symbol, isotope)}{num if num > 1 else ""}' for (symbol, isotope), num in atoms))
 
 
 def sep_formula_from_obj(m: IndigoObject, sep=' ', ordering=-1):
     smiles, *_ = m.canonicalSmiles().split(' ')
     ss = smiles.split('.')
-    ms = sorted((_indigo.loadMolecule(s) for s in ss), key=lambda tmp: ordering*tmp.molecularWeight())
+    ms = sorted((_indigo.loadMolecule(s) for s in ss), key=lambda tmp: ordering * tmp.molecularWeight())
     return sep.join((formula_from_obj(m) for m in ms))
 
 
 def sep_formula_from_smiles(smiles, sep=' ', ordering=-1):
     ss = smiles.split('.')
-    ms = sorted((_indigo.loadMolecule(s) for s in ss), key=lambda m: ordering*m.molecularWeight())
+    ms = sorted((_indigo.loadMolecule(s) for s in ss), key=lambda m: ordering * m.molecularWeight())
     return sep.join((formula_from_obj(m) for m in ms))
--- tests/test_formula.py
+++ tests/test_formula.py
@@ -13,7 +13,7 @@ class TestFormulaMethods(unittest.TestCase):
         m = _indigo.loadMolecule(smiles)
         self.assertEqual(
             sep_formula_from_smiles(smiles),
-            'C2[13C]H5DN[15N]O7PSNa HCl'
+            'C2[13C]H5DN[15N]NaO7PS HCl'
         )
 
     def test_formula_from_obj(self):
@@ -21,7 +21,7 @@ class TestFormulaMethods(unittest.TestCase):
         m = _indigo.loadMolecule(smiles)
         self.assertEqual(
             formula_from_obj(m),
-            'C2[13C]H6DClN[15N]O7PSNa'
+            'C2[13C]H6DClN[15N]NaO7PS'
         )
 
     def test_sep_formula_from_obj(self):
