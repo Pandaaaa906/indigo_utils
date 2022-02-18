690198e6f0908f3aa01950f1bcee7ae7e70b5623
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2022-02-18 13:59:01 +0800

    release 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="indigo_utils",
-    version="0.0.2",
+    version="0.0.3",
     author="Pandaaaa906",
     author_email="ye.pandaaaa906@gmail.com",
     description="A small useful tools for epam.indigo",
